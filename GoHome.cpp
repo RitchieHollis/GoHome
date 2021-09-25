9c5830d7ec5bf903dc78525bfa64760accae5fa4
RitchieHollis <84478419+RitchieHollis@users.noreply.github.com>
2021-09-25 18:50:37 +0200

    Update GoHome.cpp

--- GoHome.cpp
+++ GoHome.cpp
@@ -1,5 +1,4 @@
-// GoHome.cpp : This file contains the 'main' function. Program execution begins and ends there.// StaleInstancje.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
+// GoHome.cpp : This file contains the 'main' function. Program execution begins and ends there
 
 #include <iostream>
 #include <string>
